0f1bac5e28fa7fcd34b15224d63d6be948d3a0c1
hobin <hobin505@gmail.com>
2025-02-01 22:03:20 +0900

    feat: keyword 생성

---
+++ src/components/main/Keyword.tsx
@@ -0,0 +1,312 @@
+import React from "react";
+import "../../styles/keyword.scss";
+
+function Keyword() {
+  return (
+    <div className="keywords">
+      <div className="animation-up-late">
+        <h2 className="screen-out">브런치 키워드 영역</h2>
+        <h3
+          className="txt-brunch tit-brunch"
+          style={{
+            width: "266px",
+            height: "13px",
+            marginTop: "152px",
+            backgroundPosition: "-101px -300px",
+          }}
+        >
+          BRUNCH KEYWORD
+        </h3>
+        <p className="desc-brunch">
+          <span
+            className="txt-brunch"
+            style={{
+              width: "149px",
+              height: "11px",
+              backgroundPosition: "-175px 0px",
+            }}
+          >
+            키워드로 분류된 다양한 글 모음
+          </span>
+        </p>
+        <div className="keyword-list-wrap">
+          <div className="keyword-list" style={{ height: "360px" }}>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EC%A7%80%EA%B5%AC%ED%95%9C%EB%B0%94%ED%80%B4_%EC%84%B8%EA%B3%84%EC%97%AC%ED%96%89?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "0px", top: "0px" }}
+            >
+              <span className="keyword-item-txt">
+                지구한바퀴
+                <br />
+                세계여행
+              </span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EA%B7%B8%EB%A6%BC%C2%B7%EC%9B%B9%ED%88%B0?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "120px", top: "0px" }}
+            >
+              <span className="keyword-item-txt">그림·웹툰</span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EC%95%8C%EC%88%98%EB%A1%9D_%EC%A2%8B%EC%9D%80_%EA%B2%BD%EC%A0%9C?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "240px", top: "0px" }}
+            >
+              <span className="keyword-item-txt">알수록 좋은 경제</span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/IT_%ED%8A%B8%EB%A0%8C%EB%93%9C?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "360px", top: "0px" }}
+            >
+              <span className="keyword-item-txt">
+                IT <br /> 트렌드
+              </span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EC%82%AC%EC%A7%84%C2%B7%EC%B4%AC%EC%98%81?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "480px", top: "0px" }}
+            >
+              <span className="keyword-item-txt">사진·촬영</span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EC%B7%A8%ED%96%A5%EC%A0%80%EA%B2%A9_%EC%98%81%ED%99%94_%EB%A6%AC%EB%B7%B0?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "600px", top: "0px" }}
+            >
+              <span className="keyword-item-txt">
+                취향저격 <br />
+                영화 리뷰
+              </span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EC%98%A4%EB%8A%98%EC%9D%80_%EC%9D%B4%EB%9F%B0_%EC%B1%85?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "720px", top: "0px" }}
+            >
+              <span className="keyword-item-txt">
+                오늘은 <br />
+                이런 책
+              </span>
+              <span className="ico-brunch"></span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EB%AE%A4%EC%A7%81_%EC%9D%B8%EC%82%AC%EC%9D%B4%EB%93%9C?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "840px", top: "0px" }}
+            >
+              <span className="keyword-item-txt">뮤직 인사이드</span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EA%B8%80%EC%93%B0%EA%B8%B0_%EC%BD%94%EC%B9%98?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "0px", top: "120px" }}
+            >
+              <span className="keyword-item-txt">
+                글쓰기
+                <br />
+                코치
+              </span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EC%A7%81%EC%9E%A5%EC%9D%B8_%ED%98%84%EC%8B%A4_%EC%A1%B0%EC%96%B8?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "120px", top: "120px" }}
+            >
+              <span className="keyword-item-txt">
+                직장인
+                <br />
+                현실조언
+              </span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EC%8A%A4%ED%83%80%ED%8A%B8%EC%97%85_%EA%B2%BD%ED%97%98%EB%8B%B4?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "240px", top: "120px" }}
+            >
+              <span className="keyword-item-txt">
+                스타트업
+                <br />
+                경험담
+              </span>
+              <span className="ico-brunch"></span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EC%9C%A1%EC%95%84_%EC%9D%B4%EC%95%BC%EA%B8%B0?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "360px", top: "120px" }}
+            >
+              <span className="keyword-item-txt">
+                육아
+                <br />
+                이야기
+              </span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EC%9A%94%EB%A6%AC%C2%B7%EB%A0%88%EC%8B%9C%ED%94%BC?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "480px", top: "120px" }}
+            >
+              <span className="keyword-item-txt">요리·레시피</span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EA%B1%B4%EA%B0%95%C2%B7%EC%9A%B4%EB%8F%99?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "600px", top: "120px" }}
+            >
+              <span className="keyword-item-txt">건강·운동</span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EB%A9%98%ED%83%88_%EA%B4%80%EB%A6%AC_%EC%8B%AC%EB%A6%AC_%ED%83%90%EA%B5%AC?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "720px", top: "120px" }}
+            >
+              <span className="keyword-item-txt">
+                멘탈 관리
+                <br />
+                심리 탐구
+              </span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EB%94%94%EC%9E%90%EC%9D%B8_%EC%8A%A4%ED%86%A0%EB%A6%AC?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "840px", top: "120px" }}
+            >
+              <span className="keyword-item-txt">
+                디자인
+                <br />
+                스토리
+              </span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EB%AC%B8%ED%99%94%C2%B7%EC%98%88%EC%88%A0?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "0px", top: "240px" }}
+            >
+              <span className="keyword-item-txt">문화·예술</span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EA%B1%B4%EC%B6%95%C2%B7%EC%84%A4%EA%B3%84?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "120px", top: "240px" }}
+            >
+              <span className="keyword-item-txt">건축·설계</span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EC%9D%B8%EB%AC%B8%ED%95%99%C2%B7%EC%B2%A0%ED%95%99?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "240px", top: "240px" }}
+            >
+              <span className="keyword-item-txt">인문학·철학</span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EC%89%BD%EA%B2%8C_%EC%9D%BD%EB%8A%94_%EC%97%AD%EC%82%AC?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "360px", top: "240px" }}
+            >
+              <span className="keyword-item-txt">
+                쉽게 읽는
+                <br />
+                역사
+              </span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EC%9A%B0%EB%A6%AC%EC%A7%91_%EB%B0%98%EB%A0%A4%EB%8F%99%EB%AC%BC?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "480px", top: "240px" }}
+            >
+              <span className="keyword-item-txt">
+                우리집
+                <br />
+                반려동물
+              </span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EB%A9%8B%EC%A7%84_%EC%BA%98%EB%A6%AC%EA%B7%B8%EB%9E%98%ED%94%BC?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "600px", top: "240px" }}
+            >
+              <span className="keyword-item-txt">
+                멋진
+                <br />
+                캘리그래피
+              </span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EC%82%AC%EB%9E%91%C2%B7%EC%9D%B4%EB%B3%84?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "720px", top: "240px" }}
+            >
+              <span className="keyword-item-txt">사랑·이별</span>
+            </a>
+            <a
+              className="keyword-item Brunch-keyword-item"
+              href="https://brunch.co.kr/keyword/%EA%B0%90%EC%84%B1_%EC%97%90%EC%84%B8%EC%9D%B4?q=g"
+              target="_blank"
+              rel="noreferrer"
+              style={{ left: "840px", top: "240px" }}
+            >
+              <span className="keyword-item-txt">
+                감성
+                <br />
+                에세이
+              </span>
+              <span className="ico-brunch"></span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Keyword;
