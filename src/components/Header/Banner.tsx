bd7a595d00da0a9caa4244f9c6700dfc99e62472
hobin <hobin505@gmail.com>
2025-03-03 22:41:07 +0900

    fix: banner 효과 생성

--- src/components/Header/Banner.tsx
+++ src/components/Header/Banner.tsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/banner.scss";
-import * as Images from "../../assets";
+import * as Images from "../../types/index";
 
 const Banner = () => {
+  const [VisibleBanner, setVisibleBanner] = useState<string>("0px");
+
+  const closeBanner = () => {
+    setVisibleBanner("-480px");
+  };
+
+  useEffect(() => {
+    const handleScroll = (e: WheelEvent) => {
+      // 현재 wheel이 올라갔는지 파악하기 위해 deltaY 사용
+      if (window.scrollY === 0 && e.deltaY < 0) {
+        setVisibleBanner("0px");
+      }
+    };
+
+    window.addEventListener("wheel", handleScroll);
+    return () => window.removeEventListener("wheel", handleScroll);
+  }, []);
+
   return (
     <>
-      <div className="Image-container">
-        <img src={Images.banner} alt="banner" className="Image" />
-        <img src={Images.bannertext} alt="bannertext" className="Image-text" />
-        <button className="Close-button">
-          <img src={Images.cancel} alt="cancel" className="Close-button" />
-        </button>
+      <div className="wrap-banner" style={{ marginTop: VisibleBanner }}>
+        <div className="image-container">
+          <img src={Images.banner} alt="banner" className="image" />
+          <img
+            src={Images.bannertext}
+            alt="bannertext"
+            className="image-text"
+          />
+          <button className="close-button" onClick={closeBanner}>
+            <img src={Images.cancel} alt="cancel" className="close-button" />
+          </button>
+        </div>
       </div>
-      <div className="Small-image-container">
+      <div className="small-image-container">
         <a
           href="https://brunch.co.kr/@brunch/370?t_src=brunch_pc_topbanner"
           target="_blank"
@@ -22,12 +46,12 @@ const Banner = () => {
           <img
             src={Images.smallbanner}
             alt="smallbanner"
-            className="Small-banner"
+            className="small-banner"
           />
           <img
             src={Images.smallbannertext}
             alt="smallbannertext"
-            className="Small-banner-text"
+            className="small-banner-text"
           />
         </a>
       </div>
