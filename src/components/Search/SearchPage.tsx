66386b9ec14272aff1383d78d34e6c3a789a1324
hobin <hobin505@gmail.com>
2025-03-03 22:56:43 +0900

    fix: 검색페이지 내용 변경

--- src/components/Search/SearchPage.tsx
+++ src/components/Search/SearchPage.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import "../../styles/searchPage.scss";
-import * as Images from "../../assets";
+import * as Images from "../../types";
 import SideMenu from "../Header/SideMenu";
+import { searchWriter } from "../../data/searchWriter";
 
 interface SearchPageProps {
   onClose: () => void;
@@ -30,12 +31,12 @@ function SearchPage({ onClose }: SearchPageProps) {
               brunch<span className="story">story</span>
             </p>
           </div>
-          <button className="close-button" onClick={onClose}>
+          <button className="search-close-button" onClick={onClose}>
             <span className="ico_close">×</span>
           </button>
         </div>
         <div className="search-header">
-          <h3 className="screen-out">검색어를 입력해주세요</h3>
+          <h3 className="screen-out">검색어를 입력해주세요.</h3>
           <div className="search-input-wrapper">
             <input
               type="text"
@@ -43,15 +44,35 @@ function SearchPage({ onClose }: SearchPageProps) {
               placeholder="검색어를 입력해주세요"
               autoFocus
             />
+            <button className="search-button">
+              <img src={Images.search} alt="search" className="search" />
+            </button>
           </div>
           <div className="search-tags">
-            <span className="tag-text">사진</span>
-            <span>찍는 작가들을 소개합니다.</span>
+            <span className="tag-text">책을</span>
+            <span>사랑하는 작가들을 소개합니다.</span>
           </div>
           <div className="tag-buttons">
             <button className="tag-button">글자이야기</button>
             <button className="tag-button">작가자료실</button>
             <button className="tag-button">브런치그래퍼</button>
+          </div>
+        </div>
+
+        <div className="search-writers">
+          <div className="writers-list">
+            {searchWriter.map((writer, index) => (
+              <a href="#" key={index} className="search-writer-item">
+                <div
+                  className="circle-frame"
+                  style={{ backgroundColor: writer.profileImage }}
+                ></div>
+                <div className="writer-info">
+                  <h4 className="writer-name">{writer.name}</h4>
+                  <p className="writer-description">{writer.subDescription}</p>
+                </div>
+              </a>
+            ))}
           </div>
         </div>
       </div>
