ad7de46d5dcffb4143159d1bf0e27f4d5f1d5aa0
hobin <hobin505@gmail.com>
2025-03-03 22:52:03 +0900

    feat: App.tsx에 주요 컴포넌트 추가 및 라우팅 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Banner from "./components/Header/Banner";
 import Tab from "./components/Header/Tab";
 import Intro from "./components/main/Intro";
 import Slider from "./components/main/Slider";
 import Keyword from "./components/main/Keyword";
+import Serial from "./components/main/Serial";
+import Writer from "./components/main/Writer";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <>
-      <Banner />
-      <Tab />
-      <Intro />
-      <Slider />
-      <Keyword />
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              {[
+                Banner,
+                Tab,
+                Intro,
+                Slider,
+                Keyword,
+                Serial,
+                Writer,
+                Footer,
+              ].map((Component, index) => (
+                <Component key={index} />
+              ))}
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
